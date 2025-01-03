1b51ac46b573234cdfe432d31579aa5954d19622
Julia Kuzmina <veh.gerli@mail.ru>
2025-01-03 21:40:29 +0300

    feat: done 50%

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let arr = '';
+  members.forEach(item => {
+    if (typeof item !== 'string') return false;
+    arr += item.trim()[0].toUpperCase();
+  })
+
+  return arr.split('').sort().join('');
 }
 
 module.exports = {

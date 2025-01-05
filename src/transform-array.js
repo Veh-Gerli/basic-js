cb367ce2e8724e199c2eb0e7632cf61f3fb35182
Julia Kuzmina <veh.gerli@mail.ru>
2025-01-06 02:49:35 +0300

    feat: other resolve task6

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,56 +14,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-    try {
-        if (!Array.isArray(arr))
-        throw `'arr' parameter must be an instance of the Array!`;
-    }
-    catch (err) {
-        return err;
-    }
+    // try {
+    //     if (!Array.isArray(arr))
+    //     throw `'arr' parameter must be an instance of the Array!`;
+    // }
+    // catch (err) {
+    //     return err;
+    // }
     
-    let resArr = arr.slice();
-    let notChange = -2;
-    for (let i=0; i<resArr.length; i++) {
-        if (resArr[i] === '--double-next' ) { 
-            if (notChange !== i+1 && i < resArr.length - 1 ) {
-            resArr[i] = resArr[i + 1];
-            notChange = i;
-            }
-            else  resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
-        }
-        if (resArr[i] === '--double-prev' ) {
-            console.log(resArr, resArr[i], i, notChange)
-            if (notChange !== i-1 && i > 0 ) {
-            resArr[i] = resArr[i - 1];
-            notChange = i;
-            }
-            else resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
-        }
-        if (resArr[i] === '--discard-prev') {
-              console.log(resArr, resArr[i], i, notChange)
-            if ( i > 0  && notChange !== i-1)
-             {resArr = resArr.slice(0, i-1).concat(resArr.slice(i + 1));
-              console.log(resArr, resArr.length);
-              notChange = i+1;
-              i--;
-             }
-             else  resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
-         }
+    // let resArr = arr.slice();
+    // let notChange = -2;
+    // for (let i=0; i<resArr.length; i++) {
+    //     if (resArr[i] === '--double-next' ) { 
+    //         if (notChange !== i+1 && i < resArr.length - 1 ) {
+    //         resArr[i] = resArr[i + 1];
+    //         notChange = i;
+    //         }
+    //         else  resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
+    //     }
+    //     if (resArr[i] === '--double-prev' ) {
+    //         console.log(resArr, resArr[i], i, notChange)
+    //         if (notChange !== i-1 && i > 0 ) {
+    //         resArr[i] = resArr[i - 1];
+    //         notChange = i;
+    //         }
+    //         else resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
+    //     }
+    //     if (resArr[i] === '--discard-prev') {
+    //           console.log(resArr, resArr[i], i, notChange)
+    //         if ( i > 0  && notChange !== i-1)
+    //          {resArr = resArr.slice(0, i-1).concat(resArr.slice(i + 1));
+    //           console.log(resArr, resArr.length);
+    //           notChange = i+1;
+    //           i--;
+    //          }
+    //          else  resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
+    //      }
          
-        if (resArr[i] === '--discard-next') {
-            if (i < resArr.length - 1  && notChange !== i+1) {
-             resArr = resArr.slice(0, i).concat(resArr.slice(i + 2));
-             notChange = i-1;
-             i--;
-            }  else  resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
+    //     if (resArr[i] === '--discard-next') {
+    //         if (i < resArr.length - 1  && notChange !== i+1) {
+    //          resArr = resArr.slice(0, i).concat(resArr.slice(i + 2));
+    //          notChange = i-1;
+    //          i--;
+    //         }  else  resArr = resArr.slice(0, i).concat(resArr.slice(i + 1));
             
-            console.log(resArr, resArr[i], i, notChange)
-         }
+    //         console.log(resArr, resArr[i], i, notChange)
+    //      }
+    // }
+    
+    
+    // return resArr;
+
+        // Check if the input is an array
+    if (!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!");
     }
-    
-    
-    return resArr;
+
+    const result = [];
+    const length = arr.length;
+
+    for (let i = 0; i < length; i++) {
+        const current = arr[i];
+
+        // Handle control sequences
+        if (current === '--discard-next') {
+            // Skip the next element
+            i++;
+        } else if (current === '--discard-prev') {
+            // Discard the previous element if it's valid
+            if (i > 0) {
+                result.pop();
+            }
+        } else if (current === '--double-next') {
+            // Double the next element if it's valid
+            if (i < length - 1) {
+                result.push(arr[i + 1]);
+            }
+        } else if (current === '--double-prev') {
+            // Double the previous element if it's valid
+            if (i > 0) {
+                result.push(arr[i - 1]);
+            }
+        } else {
+            // Push the current element to the result if it's not a control sequence
+            result.push(current);
+        }
+    }
+
+    return result;
 }
 
 module.exports = {

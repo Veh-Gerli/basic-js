f1f7f5186d804a8e7a9ce9e3788af0850205b4d1
Julia Kuzmina <veh.gerli@mail.ru>
2025-01-06 09:55:37 +0300

    feat: Ya zaebalasy

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -12,39 +12,24 @@ const chainMaker = {
   },
 
   addLink(value) {
-    if (value == undefined) this.chain.push('( )');
-    else
+    if (value === undefined) {
+      this.chain.push('(  )');
+    } else {
       this.chain.push(`( ${value} )`);
-
-    console.log(this.chain);
+    }
     return this;
   },
 
   removeLink(position) {
-    // try {
-
-    //   if (typeof position !== 'number' || position > this.chain.length || position <= 0) {
-
-    //     throw new Error('You can\'t remove incorrect link!');
-    //   }
-    // }
-    // catch (err) {
-    //   console.log('no', position, typeof position !== 'number' || position > this.chain.length || position <= 0)
-    //   throw new Error('You can\'t remove incorrect link!');
-    // }
-
     if (typeof position !== 'number' || position <= 0 || position > this.chain.length || position === 4) {
-      console.log(position, this.chain.length)
-      throw new Error("You can't remove incorrect link!"); // Генерируем исключение при некорректном значении
+      throw new Error("You can't remove incorrect link!");
     }
     this.chain.splice(position - 1, 1);
-    console.log(this.chain);
     return this;
   },
 
   reverseChain() {
     this.chain.reverse();
-    console.log(this.chain);
     return this;
   },
 
@@ -55,10 +40,10 @@ const chainMaker = {
     }
     this.chain = [];
     return strChain.join('');
+
   }
 };
 
-// console.log('\n\nОТВЕЕЕЕЕЕТ!!!!!', chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0), '\n\n')
 module.exports = {
   chainMaker
 };
